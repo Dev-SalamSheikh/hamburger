830cc0f259e5d2dc2925d9054dfb7c7b04d7ac23
Salam Sheikh <dev.salamsheikh@gmail.com>
2022-03-14 22:15:03 +0600

    final commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const burger = document.querySelector(".hamburger");
+const menu = document.querySelector(".nav-menu")
+
+burger.addEventListener("click", () => {
+    burger.classList.toggle("active")
+    menu.classList.toggle("active")
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    burger.classList.remove("active")
+    menu.classList.remove("active")
+}))
